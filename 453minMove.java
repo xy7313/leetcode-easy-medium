09bf05d0d634651eeea2d6ec4e9a275b55245830
xy7313 <syrupyang@hotmail.com>
2016-12-02 01:28:01 -0500

    453

---
+++ 453minMove.java
@@ -0,0 +1,48 @@
+//自己好不容易而写了个解，然而这个用例跑不过，其他的目前都还行[1,2147483647]，总之应该是复杂度太高吧
+public class Solution {
+    public int minMoves(int[] nums) {
+        int re = 0;
+        boolean notEqu = true;
+        boolean in = true;
+        
+        while(notEqu){
+            int a = 0;
+            for(int i = 0; i<nums.length-1;i++){
+                 if((nums[i])>Integer.MAX_VALUE){
+                    notEqu = false;
+                    break;
+                }
+                if (nums[i]!=nums[i+1]){
+                    a++;
+                    break;
+                }
+            }
+            if(a==0) notEqu = false;
+            if(notEqu==false) return re;
+            Arrays.sort(nums);
+            for(int j = 0; j<nums.length-1;j++){
+                // if (nums[j]<nums[nums.length-1]){
+                    nums[j]++;
+                // }
+            }
+            re++;
+        }
+        return re;
+    }
+}
+//大神的方法，把除最大一个元素以外都+1的概念转换成，把最大值-1,每个值和最小值的差值就是他变成最小值所需要的操作数，
+public class Solution {
+    public int minMoves(int[] nums) {
+        int re = 0;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i <nums.length;i++){
+            if(nums[i]<min){
+                min = nums[i];
+            }
+        }
+        for(int n:nums){
+            re+=(n-min);
+        }
+        return re;
+    }
+}
