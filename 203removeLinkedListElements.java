081363da23ae89f8aa8c928565bbab7733e4ed23
xy7313 <syrupyang@hotmail.com>
2017-01-05 15:32:55 -0500

    203

---
+++ 203removeLinkedListElements.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head==null) return head;
+        //while，不用if，test case：[1,1] 1
+        while(head!=null&&head.val==val) head= head.next;
+        ListNode next = head;
+        while(next!=null&&next.next!=null){
+            if(next.next.val==val) next.next = next.next.next;
+            //else 提交出错，忘了写else了。。。test case:[1,2,2,1] 2
+            else next = next.next;
+        }
+        return head;
+    }
+}
