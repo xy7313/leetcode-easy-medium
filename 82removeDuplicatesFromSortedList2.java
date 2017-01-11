f4280ff915958866d621bf50669679ed1ac02cbf
xy7313 <syrupyang@hotmail.com>
2017-01-11 16:52:08 -0500

    82

--- 82removeDuplicatesFromSortedList2.java
+++ 82removeDuplicatesFromSortedList2.java
@@ -3,23 +3,23 @@
 //这个题不会做！！第一个方法还稍微好懂一点，拿[1,1,1,2,3]的例子来说，第一个内层while结束，slow.next=head=1, fast=最后一个1， 进入if(注意并不是比较val)，
 
 public ListNode deleteDuplicates(ListNode head) {
-//use two pointers, slow - track the node before the dup nodes, 
-// fast - to find the last node of dups.
-ListNode dummy = new ListNode(0), fast = head, slow = dummy;
-slow.next = fast;
-while(fast != null) {
-	while (fast.next != null && fast.val == fast.next.val) {
-		fast = fast.next;    //while loop to find the last node of the dups.
+	//use two pointers, slow - track the node before the dup nodes, 
+	// fast - to find the last node of dups.
+	ListNode dummy = new ListNode(0), fast = head, slow = dummy;
+	slow.next = fast;
+	while(fast != null) {
+		while (fast.next != null && fast.val == fast.next.val) {
+			fast = fast.next;    //while loop to find the last node of the dups.
+		}
+		if (slow.next != fast) { //duplicates detected.
+			slow.next = fast.next; //remove the dups.
+			fast = slow.next;     //reposition the fast pointer.
+		} else { //no dup, move down both pointer.
+			slow = slow.next;
+			fast = fast.next;
+		}
 	}
-	if (slow.next != fast) { //duplicates detected.
-		slow.next = fast.next; //remove the dups.
-		fast = slow.next;     //reposition the fast pointer.
-	} else { //no dup, move down both pointer.
-		slow = slow.next;
-		fast = fast.next;
-	}
-}
-return dummy.next;
+	return dummy.next;
 }
 
 public ListNode deleteDuplicates(ListNode head) {
@@ -40,16 +40,16 @@ public ListNode deleteDuplicates(ListNode head) {
 
 //recursive solution
 public ListNode deleteDuplicates(ListNode head) {
-if (head == null) return null;
+	if (head == null) return null;
 
-if (head.next != null && head.val == head.next.val) {
-	while (head.next != null && head.val == head.next.val) {
-		head = head.next;
+	if (head.next != null && head.val == head.next.val) {
+		while (head.next != null && head.val == head.next.val) {
+			head = head.next;
+		}
+		return deleteDuplicates(head.next);
+	} else {
+		head.next = deleteDuplicates(head.next);
 	}
-	return deleteDuplicates(head.next);
-} else {
-	head.next = deleteDuplicates(head.next);
-}
-return head;
+	return head;
 }
 
