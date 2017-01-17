c77012d65b9585a4ff1b9b9e25734e7119644379
xy7313 <syrupyang@hotmail.com>
2017-01-16 23:32:26 -0500

    3

--- 3longestSubstringWithoutRepeatingCharacters.java
+++ 3longestSubstringWithoutRepeatingCharacters.java
@@ -15,7 +15,7 @@ public class Solution {
         }
         return max;
     }
-    //和上题思路一样
+    //和上题思路一样，但用的是hashmap存储数据
     public int lengthOfLongestSubstring(String s) {
             char[] cofS = s.toCharArray();
             Map<Character, Integer> m= new HashMap<Character,Integer>();
@@ -29,44 +29,28 @@ public class Solution {
             }
             return maxLen;
         }
-// public int lengthOfLongestSubstring(String s) {  
-//         int length = s.length();  
-//         if (length == 0) {  
-//             return 0;  
-//         }  
-//         int [] countTable = new int[256];  
-//         Arrays.fill(countTable, -1);  
-//         int max = 1;  
-//         int start = 0;  
-//         int end = 1;  
+        //用新数组存储，虽然看起来比较牛逼，但个人觉得不如set，hashmap这两种简明实用
+    public int lengthOfLongestSubstring(String s) {  
+        int length = s.length();  
+        if (length == 0) {  
+            return 0;  
+        }  
+        int [] countTable = new int[256];  
+        Arrays.fill(countTable, -1);  
+        int max = 1;  
+        int start = 0;  
+        int end = 1;  
           
-//         countTable[s.charAt(0)] = 0;  
-//         while (end < length) {  
-//             //Has not reached a duplicate char  
-//             if (countTable[s.charAt(end)] >= start) {  
-//                 start = countTable[s.charAt(end)] + 1;                
-//             }  
-//             max = Math.max(max, end - start + 1);  
-//             countTable[s.charAt(end)] = end;  
-//             end++;  
-//         }  
-//         return max;  
-//     }  
-    public int lengthOfLongestSubstring(String s) {
-    int start = 0, end = 0, max = 0;
-    Set<Character> set = new HashSet<>();
-    
-    while (end < s.length()) {
-        if (!set.contains(s.charAt(end))) {
-            set.add(s.charAt(end));
-            end++;
-            max = Math.max(max, set.size());
-        } else {
-            set.remove(s.charAt(start));
-            start++;
-        }
-    }
-    
-    return max;
-}
+        countTable[s.charAt(0)] = 0;  
+        while (end < length) {  
+            //Has not reached a duplicate char  
+            if (countTable[s.charAt(end)] >= start) {  
+                start = countTable[s.charAt(end)] + 1;                
+            }  
+            max = Math.max(max, end - start + 1);  
+            countTable[s.charAt(end)] = end;  
+            end++;  
+        }  
+        return max;  
+    }  
 }
